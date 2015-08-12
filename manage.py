ddddf4c0122b457225928f116b9a22850bcbb17a
FRAMGIA\le.cong.phuc <le.cong.phuc@framgia.com>
2015-08-12 09:31:17 +0700

    Change setting

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TrainingSystem.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "framgia-training-system.settings")
 
     from django.core.management import execute_from_command_line
 
