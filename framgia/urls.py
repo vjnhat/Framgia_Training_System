2ae4808da1bfc1d4452ee07e2f795184c8e18194
FRAMGIA\le.cong.phuc <le.cong.phuc@framgia.com>
2015-08-13 15:05:44 +0700

    [UPDATE] code

--- framgia/urls.py
+++ framgia/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-<<<<<<< HEAD
-from framgia.view import user, base
-=======
+
 from . import views
 
->>>>>>> [UPDATE] code
 urlpatterns = [
     url(r'^$', views.base.index, name='base_index'),
 
@@ -15,6 +12,6 @@ urlpatterns = [
     url(r'^user/course-learning/', views.user.user_learning_course, name='user_course_learning'),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/', user.login_user, name="login"),
-    url(r"^$", base.Home.as_view(), name="home"),
+
+    url(r'^login/', views.user.login_user, name="login"),
 ]
--- framgia/views/user.py
+++ framgia/views/user.py
@@ -1,6 +1,33 @@
 __author__ = 'FRAMGIA\le.cong.phuc'
 from django.shortcuts import render
+from django.shortcuts import render_to_response, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.template.context_processors import csrf
+from django.template import RequestContext
 
 
 def user_learning_course(request):
     return render(request, 'user/user_learning_course.html', {})
+
+
+def login_user(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+                HttpResponseRedirect("index.hml")
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('user/auth.html',{'state':state, 'username': username}, context_instance = RequestContext(request))
