dc61781d313d17510d95aba5bd5dfef6ec3c9d8b
Nguyenhai2310 <nguyen.khac.hai@framgia.com>
2015-08-13 13:37:21 +0700

    [Add] login, home

--- framgia/view/base.py
+++ framgia/view/base.py
@@ -1,1 +1,8 @@
 __author__ = 'FRAMGIA\le.cong.phuc'
+from django.shortcuts import HttpResponse, render
+from django.views.generic import ListView, DetailView, View
+
+
+class Home(View):
+    def get(self, request):
+        return render(request,'index.html')
