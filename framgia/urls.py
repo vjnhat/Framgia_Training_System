d2247d1bc940c4030728ca2160836d54f5e5d310
vjnhat <tran.van.nhat@framgia.com>
2015-08-18 15:23:31 +0700

    fix urls

--- framgia/urls.py
+++ framgia/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
-from framgia.views.admin.course  import CourseList, CourseDetail, CourseUpdate, CourseDelete, CourseCreate
-from framgia.views.admin.subject import SubjectCreate, SubjectDelete, SubjectDetail, SubjectList, SubjectUpdate
+
 
 urlpatterns = [
 
@@ -18,14 +17,5 @@ urlpatterns = [
     url(r'^logout/$', 'django.contrib.auth.views.logout',
                           {'next_page': '/'}, name="logout"),
     url(r'^register/$', views.user.register, name="register"),
-    url(r'^admin/courses/$',CourseList.as_view(), name = "course_index"),
-    url(r'^admin/course/(?P<pk>\d+)/$', CourseDetail.as_view(), name= "course_detail"),
-    url(r'^admin/course/(?P<pk>\d+)/edit/$', CourseUpdate.as_view(), name = "course_update"),
-    url(r'^admin/course/(?P<pk>\d+)/delete/$', CourseDelete.as_view(), name = "course_delete"),
-    url(r'^admin/course/new/$', CourseCreate.as_view(), name = "course_new" ),
-    url(r'^admin/subjects/$', SubjectList.as_view(), name = "subject_index"),
-    url(r'^admin/subject/(?P<pk>\d+)', SubjectDetail.as_view(),name = "subject_detail"),
-    url(r'admin/subject/(?P<pk>\d+)/edit/$', SubjectUpdate.as_view(), name="subject_update"),
-    url(r'admin/subject/(?P<pk>\d+)/delete/$', SubjectDelete.as_view(), name="subject_delete"),
-    url(r'admin/subject/new/$', SubjectCreate.as_view(), name="subject_new"),
+    url(r'^admin/', include('framgia.views.admin.urls')),
 ]
